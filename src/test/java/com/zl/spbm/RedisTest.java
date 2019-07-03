80b0575e421c4f67098f828183181cb265b4791f
zl_go <zl_go@foxmail.com>
2019-07-03 09:33:05 +0800

    Redssion

--- src/test/java/com/zl/spbm/RedisTest.java
+++ src/test/java/com/zl/spbm/RedisTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.redisson.RedissonClient;
 import org.redisson.core.RBlockingQueue;
-import org.redisson.core.RLock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
