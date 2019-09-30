c702430316733220541cf43f937d19ef4381f62d
lzhang <zl_go@foxmail.com>
2019-09-30 09:24:41 +0800

    slf4j+logback

--- src/main/java/com/zl/spbm/controller/QkHealthyController.java
+++ src/main/java/com/zl/spbm/controller/QkHealthyController.java
@@ -1,8 +1,11 @@
 package com.zl.spbm.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.net.InetAddress;
 
@@ -13,7 +16,11 @@ import java.net.InetAddress;
 @Controller
 @Slf4j
 public class QkHealthyController {
+
+    private final static Logger LOGGER  = LoggerFactory.getLogger(QkHealthyController.class);
+
     @RequestMapping("/ok")
+    @ResponseBody
     public String checkOk(){
         InetAddress addr;
         String ip = "";
@@ -24,6 +31,7 @@ public class QkHealthyController {
             ip = addr.getHostAddress();
             //获得机器名称
             address = addr.getHostName();
+            LOGGER.info("QkHealthyController health check. address:{},ip:{},hostName:{}",addr,ip,address);
         }catch(Exception e){
             log.error("checkOk error:{}", e);
         }
